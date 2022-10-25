ce52a8038b0d616d8d62a2b3361ff993782c9bc5
selcukarz <ariozselcuk@gmail.com>
2022-10-25 19:59:38 +0300

    first commit

---
+++ LandmarkBook/DetailVC.swift
@@ -0,0 +1,29 @@
+//
+//  DetailVC.swift
+//  LandmarkBook
+//
+//  Created by Selçuk Arıöz on 25.10.2022.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
